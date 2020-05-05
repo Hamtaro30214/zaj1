ca2809a702483bb0516391e7daf06623566615ab
Hamtaro30214 <marcin-ros98@wp.pl>
2020-05-05 15:06:23 +0200

    zad 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.io.File;
+
+public class Animal {
+    String species;
+    String name;
+    File pic;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human me = new Human();
+        me.firstName = "Marcin";
+        me.lastName = "Rosicki";
+    }
+}
