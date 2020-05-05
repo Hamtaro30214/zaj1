32a9f3d328ac7765880e3e60ca058de4b7678dd7
Hamtaro30214 <marcin-ros98@wp.pl>
2020-05-05 15:12:35 +0200

    zad 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -3,7 +3,37 @@ package com.company;
 import java.io.File;
 
 public class Animal {
-    String species;
+    final String species;
     String name;
     File pic;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        if (species == "Dod") {
+            weight = 10.0;
+        } else if (species == "Lion") {
+            weight = 190.0;
+        } else {
+            weight = 1.0;
+        }
+    }
+
+    void feed() {
+        if (weight <= 0.0) {
+            System.out.println("Nie można nakarmić martwego zwierzaka.");
+        } else {
+            weight++;
+            System.out.println("Waga zwierzaka: " + weight);
+        }
+    }
+
+    void takeForAWalk() {
+        if (weight <= 0.0) {
+            System.out.println("Nie można iść na spacer z martwym zwierzakiem.");
+        } else {
+            weight -= 0.1;
+            System.out.println("Udany spacer: " + weight);
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal lion = new Animal("Lion");
+        lion.name = "Brutus";
+
         Human me = new Human();
         me.firstName = "Marcin";
         me.lastName = "Rosicki";
+        me.pet = lion;
+
+        lion.feed();
+        me.pet.feed();
+
+        lion.takeForAWalk();
+        me.pet.takeForAWalk();
     }
 }
