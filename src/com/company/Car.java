3d3d9895a8353b3a14fa5164560fea02272a17a8
Hamtaro30214 <marcin-ros98@wp.pl>
2020-05-05 15:17:06 +0200

    zad 1

---
+++ src/com/company/Car.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Car {
+    final String producer;
+    final String model;
+    int numberOfDoors;
+    double combustion;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,16 @@ public class Main {
         Animal lion = new Animal("Lion");
         lion.name = "Brutus";
 
+        Car smallCar = new Car("Ford", "Focus");
+        smallCar.numberOfDoors = 5;
+        smallCar.combustion = 12.0;
+
         Human me = new Human();
         me.firstName = "Marcin";
         me.lastName = "Rosicki";
         me.pet = lion;
+        me.car = smallCar;
+
 
         lion.feed();
         me.pet.feed();
